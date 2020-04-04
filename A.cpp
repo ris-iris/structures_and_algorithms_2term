832962fa1375955d12a45abeac323182cac3dddf
ris-iris <sagitova.ma@phystech.edu>
2020-04-04 21:05:08 +0300

    changed A

--- A.cpp
+++ A.cpp
@@ -2,67 +2,70 @@
 #include <vector>
 #include <queue>
 
-void bfs(int v, int** timeFromV, std::vector<std::vector<int> > graph){
-    std::queue<int> q;
-    int temp;
-    int to;
-    q.push(v);
-    (*timeFromV)[v] = 0;
-    while(!q.empty()){
-        temp = q.front();
-        q.pop();
-        for(int i = 0; i < graph[temp].size(); ++i){
-            to = graph[temp][i];
-            if((*timeFromV)[to] == -1){
-                (*timeFromV)[to] = (*timeFromV)[temp] + 1;
-                q.push(to);
-            }
-        }
+void bfs(int v, std::vector<int> &timeFromV, std::vector<std::vector<int> > graph) {
+  std::queue<int> q;
+  int temp;
+  int to;
+  q.push(v);
+  timeFromV[v] = 0;
+  while (!q.empty()) {
+    temp = q.front();
+    q.pop();
+    for (int i = 0; i < graph[temp].size(); ++i) {
+      to = graph[temp][i];
+      if (timeFromV[to] == -1) {
+        timeFromV[to] = timeFromV[temp] + 1;
+        q.push(to);
+      }
     }
+  }
 }
 
+int getMinLength(std::vector<int> &t1, std::vector<int> &t2, std::vector<int> &t3) {
+  int lenght = 1000000;
+
+  for (int i = 0; i < t1.size(); ++i) {
+    lenght = std::min(t1[i] + t2[i] + t3[i], lenght);
+  }
+
+  return lenght;
+}
+
+void getGraph(std::vector<std::vector<int> > &graph, int edgeAm, std::istream &is) {
+  int a, b;
+  for (int i = 0; i < edgeAm; ++i) {
+    is >> a >> b;
+    a--;
+    b--;
+    graph[a].push_back(b);
+    graph[b].push_back(a);
+  }
+}
 
 int main() {
-    int n, m, leon, matilda, moloko, a, b;
+  int n, m, leon, matilda, moloko;
 
-    std::cin >>  n >> m >> leon >> matilda >> moloko;
-    leon--;
-    matilda--;
-    moloko--;
-    std::vector< std::vector<int> > graph(n);
-    int* timeFromLeon = new int[n];
-    int* timeFromMatilda = new int[n];
-    int* timeFromMilk = new int[n];
+  std::cin >> n >> m >> leon >> matilda >> moloko;
+  leon--;
+  matilda--;
+  moloko--;
+  std::vector<std::vector<int> > graph(n);
+  std::vector<int> timeFromLeon(n);
+  std::vector<int> timeFromMatilda(n);
+  std::vector<int> timeFromMilk(n);
 
-    for(int i = 0; i < n; ++i){
-        timeFromLeon[i] = -1;
-        timeFromMatilda[i] = -1;
-        timeFromMilk[i] = -1;
-    }
-    for(int i = 0; i < m; ++i){
-        std::cin >> a >> b;
-        a--;
-        b--;
-        graph[a].push_back(b);
-        graph[b].push_back(a);
-    }
+  for (int i = 0; i < n; ++i) {
+    timeFromLeon[i] = -1;
+    timeFromMatilda[i] = -1;
+    timeFromMilk[i] = -1;
+  }
 
-    bfs(leon, &timeFromLeon, graph);
-    bfs(matilda, &timeFromMatilda, graph);
-    bfs(moloko, &timeFromMilk, graph);
+  getGraph(graph, m, std::cin);
 
-    int lenght = 1000000;
+  bfs(leon, timeFromLeon, graph);
+  bfs(matilda, timeFromMatilda, graph);
+  bfs(moloko, timeFromMilk, graph);
 
-    for(int i = 0; i < n; ++i){
-        lenght = std::min(timeFromLeon[i] + timeFromMatilda[i] + timeFromMilk[i], lenght);
-    }
-
-    std::cout << lenght;
-
-    delete[] timeFromLeon;
-    delete[] timeFromMatilda;
-    delete[] timeFromMilk;
-
-    return 0;
-
+  std::cout << getMinLength(timeFromLeon, timeFromMatilda, timeFromMilk);
+  return 0;
 }
