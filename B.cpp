3a7dff77e58febced3ce2fd44dafed75c519e8ab
ris-iris <sagitova.ma@phystech.edu>
2020-04-04 21:06:55 +0300

    changed B

--- B.cpp
+++ B.cpp
@@ -1,50 +1,66 @@
 #include <iostream>
 #include <vector>
 
-void dfs(int v, std::vector<std::vector<int> > &graph, std::vector<int> &color, std::vector<int> &sorted, bool &hasCycle){
-    color[v] = 1;
-    for(int i = 0; i < graph[v].size(); ++i){
-        int to = graph[v][i];
-        if(color[to] == 0){
-            dfs(to, graph, color, sorted, hasCycle);
-        } else if(color[to] == 1){
-            hasCycle = true;
-        }
+enum color { WHITE, GRAY, BLACK };
+
+void dfs(int v,
+         std::vector<std::vector<int> > &graph,
+         std::vector<color> &color,
+         std::vector<int> &sorted,
+         bool &hasCycle) {
+  color[v] = GRAY;
+  for (int i = 0; i < graph[v].size(); ++i) {
+    int to = graph[v][i];
+    if (color[to] == WHITE) {
+      dfs(to, graph, color, sorted, hasCycle);
+    } else if (color[to] == GRAY) {
+      hasCycle = true;
     }
-    color[v] = 2;
-    sorted.push_back(v);
+  }
+  color[v] = BLACK;
+  sorted.push_back(v);
 }
 
-int main(){
-    int n, m, a, b;
-    std::cin >> n >> m;
-    std::vector<std::vector<int> > graph(n);
-    for (int i = 0; i < m; ++i) {
-        std::cin >> a >> b;
-        graph[a].push_back(b);
-    }
-    std::vector<int> color(n);
-    for (int i = 0; i < n; ++i) {
-        color[i] = 0;
-    }
-    std::vector<int> sorted;
+void setHasCycle(std::vector<std::vector<int> > &graph,
+                 std::vector<color> &color,
+                 std::vector<int> &sorted,
+                 bool &hasCycle) {
+  for (int i = 0; i < color.size(); ++i) {
+    if (hasCycle)
+      break;
+    if (color[i] == 0)
+      dfs(i, graph, color, sorted, hasCycle);
+  }
+}
 
-    bool hasCycle = false;
-    for (int i = 0; i < n; ++i) {
-        if(hasCycle)
-            break;
-        if(color[i] == 0)
-            dfs(i, graph, color, sorted, hasCycle);
-    }
+void getGraph(std::vector<std::vector<int> > &graph, int edgeAm, std::istream &is) {
+  int a, b;
+  for (int i = 0; i < edgeAm; ++i) {
+    is >> a >> b;
+    graph[a].push_back(b);
+  }
+}
 
-    if(hasCycle){
-        std::cout << "NO";
-    }else{
-        std::cout << "YES" << std::endl;
-        for(int i = n -1; i > -1; --i){
-            std::cout << sorted[i] << " ";
-        }
-    }
+int main() {
+  int n, m, a, b;
+  std::cin >> n >> m;
+  std::vector<std::vector<int> > graph(n);
+  getGraph(graph, m, std::cin);
 
-    return 0;
+  std::vector<color> color(n, WHITE);
+  std::vector<int> sorted;
+
+  bool hasCycle = false;
+  setHasCycle(graph, color, sorted, hasCycle);
+
+  if (hasCycle) {
+    std::cout << "NO";
+  } else {
+    std::cout << "YES" << std::endl;
+    for (int i = n - 1; i > -1; --i) {
+      std::cout << sorted[i] << " ";
+    }
+  }
+
+  return 0;
 }
