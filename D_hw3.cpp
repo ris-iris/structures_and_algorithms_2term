de98b0fe50e2760b9df7dcea7399fc1ce1b25eb3
ris-iris <sagitova.ma@phystech.edu>
2020-05-11 07:12:17 +0300

    change

--- D_hw3.cpp
+++ D_hw3.cpp
@@ -9,10 +9,10 @@ int getEdges(int n, int m, std::vector<std::vector<int> > &edges) {
   int vNum = 0;
   for (int i = 0; i < n; ++i) {
     for (int j = 0; j < m; ++j) {
-      std::cin>> ch;
-      if(ch == '.')
+      std::cin >> ch;
+      if (ch == '.') {
         bridge[i].push_back(-1);
-      else{
+      } else {
         bridge[i].push_back(vNum);
         ++vNum;
       }
@@ -21,8 +21,8 @@ int getEdges(int n, int m, std::vector<std::vector<int> > &edges) {
   edges.resize(vNum);
   for (int i = 1; i < n; ++i) {
     for (int j = 0; j < m; ++j) {
-      if(bridge[i][j] > -1){
-        if (bridge[i - 1][j] > -1){
+      if (bridge[i][j] > -1) {
+        if (bridge[i - 1][j] > -1) {
           edges[bridge[i][j]].push_back(bridge[i - 1][j]);
           edges[bridge[i - 1][j]].push_back(bridge[i][j]);
         }
@@ -31,8 +31,8 @@ int getEdges(int n, int m, std::vector<std::vector<int> > &edges) {
   }
   for (int i = 0; i < n; ++i) {
     for (int j = 1; j < m; ++j) {
-      if(bridge[i][j] > -1){
-        if (bridge[i][j - 1] > -1){
+      if (bridge[i][j] > -1) {
+        if (bridge[i][j - 1] > -1) {
           edges[bridge[i][j]].push_back(bridge[i][j - 1]);
           edges[bridge[i][j - 1]].push_back(bridge[i][j]);
         }
@@ -42,13 +42,14 @@ int getEdges(int n, int m, std::vector<std::vector<int> > &edges) {
   return vNum;
 }
 
-bool dfs(int v, std::vector<std::vector<int> >& graph, std::vector<int>& match, std::vector<bool>& visited){
-  if(visited[v])
+bool dfs(int v, std::vector<std::vector<int> > &graph, std::vector<int> &match, std::vector<bool> &visited) {
+  if (visited[v]) {
     return false;
+  }
   visited[v] = true;
   for (int i = 0; i < graph[v].size(); ++i) {
     int to = graph[v][i];
-    if(match[to] == -1 || dfs(match[to], graph, match, visited)){
+    if (match[to] == -1 || dfs(match[to], graph, match, visited)) {
       match[to] = v;
       return true;
     }
@@ -64,14 +65,18 @@ int main() {
   std::vector<int> match(size, -1);
   std::vector<bool> visited(size);
   for (int i = 0; i < size; ++i) {
-    visited.assign(size,false);
+    visited.assign(size, false);
     dfs(i, graph, match, visited);
   }
   int countB = 0;
   for (int i = 0; i < size; ++i) {
-    if(match[i] == -1) ++countB;
+    if (match[i] == -1) {
+      ++countB;
+    }
   }
-  if(a >= 2*b) countB = size;
+  if (a >= 2 * b) {
+    countB = size;
+  }
   std::cout << countB * b + (size - countB) * a / 2;
   return 0;
 }
